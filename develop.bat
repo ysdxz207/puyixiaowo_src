3065111bd471a043d210ed7df917f745336e8f74
ysdxz207 <ysdxz207@qq.com>
2017-04-18 15:28:36 +0800

    auto commit

--- develop.bat
+++ develop.bat
@@ -4,7 +4,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 set SITE_NAME=puyixiaowo
 set GRUNT_TASK=lunr-search
 set SITE_DIR=..\puyixiaowo
-set SITE_IGNORE=(.git README.md)
+set SITE_IGNORE=.git README.md
 
 
 
@@ -13,17 +13,39 @@ set SITE_IGNORE=(.git README.md)
 set TEMP_DIR= %temp%\%SITE_NAME%
 
 ::delete public dir
-rd public /s /q
-::compass scss and regenerate search.json
-echo grunt %GRUNT_TASK%
-::generate static html
-echo hugo
-::clean site dir
-rd %TEMP_DIR% /s /q
 
-for %%a in %SITE_IGNORE% do (
-	xcopy %SITE_DIR%\%%a\*.* %TEMP_DIR%\%%a\*.* /E /K
+echo delete public dir...
+if exist "public" (
+	rd public /s /q
 )
+
+::install dependencies
+echo installing dependencies...
+call npm install
+::compass scss and regenerate search.json
+echo compass scss and regenerate search.json...
+call grunt %GRUNT_TASK%
+::generate static html
+echo generate static html...
+call hugo
+::clean site dir
+echo clean site dir...
+for /f "eol=: delims=" %%F in ('dir /b /a "%SITE_DIR%" ^| findstr /vib "%SITE_IGNORE%"') do (
+	set filename=%SITE_DIR%\%%F
+	del "!filename!" /q
+	if exist "!filename!" (
+		rd "!filename!" /s /q
+	)
+)
+::copy public/ to site dir
+echo copy public/ to site dir...
+xcopy public\*.* %SITE_DIR%\*.* /E
+
+::commit files
+call git add .
+call git commit -m "auto commit"
+call git push
+
 ENDLOCAL
 
 pause
